096a32b1ced4e4e04a7f63325baac486228f1846
NadavRuni <156665035+NadavRuni@users.noreply.github.com>
2024-02-08 18:33:50 +0200

    1

--- LoanCalc.java
+++ LoanCalc.java
@@ -1,66 +1,68 @@
-/**
-* Computes the periodical payment necessary to re-pay a given loan.
-*/
 public class LoanCalc {
-	
-	static double epsilon = 0.001;  // The computation tolerance (estimation error)
-	static int iterationCounter;    // Monitors the efficiency of the calculation
-	
-    /** 
-     * Gets the loan data and computes the periodical payment.
-     * Expects to get three command-line arguments: sum of the loan (double),
-     * interest rate (double, as a percentage), and number of payments (int).  
-     */
-	public static void main(String[] args) {		
-		// Gets the loan data
+	static double epsilon = 0.001;
+	static int iterationCounter;
+
+	public static void main(String[] args) {
+
 		double loan = Double.parseDouble(args[0]);
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
 		System.out.println("Loan sum = " + loan + ", interest rate = " + rate + "%, periods = " + n);
-		
-		// Computes the periodical payment using brute force search
+
 		System.out.print("Periodical payment, using brute force: ");
 		System.out.printf("%.2f", bruteForceSolver(loan, rate, n, epsilon));
 		System.out.println();
 		System.out.println("number of iterations: " + iterationCounter);
+		iterationCounter = 0; // Reset iteration counter
 
-		// Computes the periodical payment using bisection search
 		System.out.print("Periodical payment, using bi-section search: ");
 		System.out.printf("%.2f", bisectionSolver(loan, rate, n, epsilon));
 		System.out.println();
 		System.out.println("number of iterations: " + iterationCounter);
 	}
-	
-	/**
-	* Uses a sequential search method  ("brute force") to compute an approximation
-	* of the periodical payment that will bring the ending balance of a loan close to 0.
-	* Given: the sum of the loan, the periodical interest rate (as a percentage),
-	* the number of periods (n), and epsilon, a tolerance level.
-	*/
-	// Side effect: modifies the class variable iterationCounter.
-    public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {  
-    	// Replace the following statement with your code
-    	return 0;
-    }
-    
-    /**
-	* Uses bisection search to compute an approximation of the periodical payment 
-	* that will bring the ending balance of a loan close to 0.
-	* Given: the sum of theloan, the periodical interest rate (as a percentage),
-	* the number of periods (n), and epsilon, a tolerance level.
-	*/
-	// Side effect: modifies the class variable iterationCounter.
-    public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-    	// Replace the following statement with your code
-    	return 0;
-    }
-	
-	/**
-	* Computes the ending balance of a loan, given the sum of the loan, the periodical
-	* interest rate (as a percentage), the number of periods (n), and the periodical payment.
-	*/
+
+	public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
+		double a = loan / n;
+		double balance = endBalance(loan, rate, n, a);
+
+		while (balance > 0) {
+			a = a + epsilon;
+			balance = endBalance(loan, rate, n, a);
+			iterationCounter++;
+		}
+		return a - epsilon;
+	}
+
+	public static double bisectionSolver(double loan, double rate, int n, double epsilon) {
+		double low = loan / n;
+		double high = loan;
+		double mid = (high + low) / 2;
+		double did = endBalance(loan, rate, n, mid);
+
+		while (Math.abs(did) > epsilon) {
+			if (did > 0) {
+				low = mid;
+
+			} else {
+				high = mid;
+			}
+			mid = (low + high) / 2;
+			did = endBalance(loan, rate, n, mid);
+			iterationCounter++;
+
+		}
+		return mid;
+	}
+
 	private static double endBalance(double loan, double rate, int n, double payment) {
-		// Replace the following statement with your code
-    	return 0;
+
+		int i = 1;
+		while (i <= n) {
+			loan = (loan - payment);
+			loan = loan * ((rate / 100) + 1);
+			i++;
+
+		}
+		return loan;
 	}
 }
